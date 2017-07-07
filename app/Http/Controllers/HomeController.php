f820b7681d124e247d39ae483b3c1ace1f4beade
chayawee chueapanya <chayawee_ch59@live.rmutl.ac.th>
2017-07-07 13:40:01 +0700

    addข้อมูลเพิ่ม

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -10,4 +10,20 @@ class HomeController extends Controller
     {
         return view('index'); //ทุกฟังก์ชั่นจะต้อง return ค่าออกไปเท่านั้น
     }
+    public function create(){
+        return view('create');
+    }
+    public function store(Request $request){
+
+        return redirect('/');
+    }
+    public function edit($id){
+
+        return view('edit');
+    }
+    public function update(Request $request, $id)
+    {
+
+        return redirect('/');
+    }
 }
